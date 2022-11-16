f82a418a112ebae8b7063fc984b8eed0db95591e
王依仁 <41071101h@gapps.ntnu.edu.tw>
2022-11-17 01:16:46 +0800

    work4

--- 1020/work3.py
+++
@@ -1,9 +0,0 @@
-#抓取網頁原始碼
-import urllib.request as req
-url = "https://bds.oia.ntnu.edu.tw/istudent/OE/exchange-programs"
-request = req.Reqest(url, headers = {
-    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36"
-})
-with req.urlopen(request) as response :
-    data = response.read().decode("utf-8")
-print(data)
